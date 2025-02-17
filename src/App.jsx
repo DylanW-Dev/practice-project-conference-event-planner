0e6ef3b1d8d4f7e2de892652bb98eb9795be3eaa
DylanW-Dev <dylan.wanten.dev@gmail.com>
2025-02-17 21:29:54 +0100

    Boilerplates setups and Redux implementations

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import AboutUs from './Components/AboutUs'
+import ConferenceEvent from './Components/ConferenceEvent'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showVenue, setShowVenue] = useState(false);
+
+  const handleGetStarted = () => {
+    setShowVenue(true);
+  };
 
   return (
     <>
-      
+      <header className='first_page'>
+        <div className='main_event'>
+          <div className='first_page_name_btn'>
+            <h1 className='budget_heading'>Conference Expense Planner</h1>
+            <p className='budget_sentence'>Plan your next major event with us!</p>
+            <div className='get-started-btn'>
+              <button onClick={() => handleGetStarted()} className='get-started-btn'>
+                Get Started
+              </button>
+            </div>
+          </div>
+          <div className='aboutus_main'>
+            <AboutUs/>
+          </div>
+        </div>
+      </header>
+
+      <div className={`event-list-container ${showVenue ? 'visible' : ''}`}>
+        <ConferenceEvent/>
+      </div>
     </>
   )
 }
---
+++ src/Components/ConferenceEvent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './ConferenceEvent.css'
+
+
+const ConferenceEvent = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default ConferenceEvent
